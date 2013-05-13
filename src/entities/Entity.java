3a16d8305152783d454f83e1cc34a60e7f48089a
Josh Hofing <joshhofing@gmail.com>
2013-05-13 08:18:42 -0400

    Add width and height to Entity

--- src/entities/Entity.java
+++ src/entities/Entity.java
@@ -5,10 +5,17 @@ package edu.stuy.starlorn.entities;
  */
 public class Entity {
     private double _x, _y;
-    
-    public Entity(double x, double y) {
+    private int _width, _height;
+
+    public Entity(double x, double y, int width, int height) {
         _x = x;
         _y = y;
+        _width = width;
+        _height = height;
+    }
+
+    public Entity(double x, double y) {
+        this(x, y, 0, 0);
     }
 
     public Entity() {
@@ -47,6 +54,20 @@ public class Entity {
     }
     public double getY() {
         return _y;
+    }
+
+    public void setWidth(int width) {
+        _width = width;
+    }
+    public int getWidth() {
+        return _width;
+    }
+
+    public void setHeight(int height) {
+        _height = height;
+    }
+    public int getHeight() {
+        return _height;
     }
 
     public void setXY(double x, double y) {
