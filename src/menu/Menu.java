8c4e8c7d66d0ddad521e741c04d8f8fa561bdf30
VjiaoBlack <VjiaoBlack@gmail.com>
2013-05-14 20:12:41 -0300

    whoops, tiny bug. updated menu.java

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -110,12 +110,12 @@ public class Menu {
 		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 
 		// R,G,B,A Set The Color To Blue One Time Only
-		GL11.glColor3f(0.5f, 1.0f, 0.5f);
-
-		// draw quad
+		GL11.glColor3f(1f, 1.0f, 1f);
 		for (Rectangle x: _stars){
 			x.draw();
 		}
+		
+		
 		if (_button[0].isPressed()){
 			GL11.glColor3d(.5, 0, 0);
 		}
