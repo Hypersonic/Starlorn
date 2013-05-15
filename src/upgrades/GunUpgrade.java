1915a51acdfc48bda7d46364bcb5fb445ce5e811
Josh Hofing <joshhofing@gmail.com>
2013-05-15 10:13:20 -0400

    Get rid of static in GunUpgrade

--- src/upgrades/GunUpgrade.java
+++ src/upgrades/GunUpgrade.java
@@ -1,7 +1,7 @@
 package edu.stuy.upgrades;
 
 /*
- * Static methods because you don't need an instance, just values.
+ * Upgrades that modify a ship's gun/shots
  */
 public class GunUpgrade extends Upgrade {
 
@@ -9,13 +9,13 @@ public class GunUpgrade extends Upgrade {
      * Gives an angle this ship should aim at, measured in radians,
      * From the vertical axis.
      */
-    public static double getAimAngle() {
+    public double getAimAngle() {
         return 0; // Aim straight.
     }
     /* 
      * Given the amount of damage that would be done, tell how much should be done.
      */
-    public static int getDamage(int damage) {
+    public int getDamage(int damage) {
         return damage;
     }
 }
