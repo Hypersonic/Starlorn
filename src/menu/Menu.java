ff810b15eaebec6f8a18c199a7baaa32bc39e4b2
Victor Jiao <VjiaoBlack@gmail.com>
2013-05-15 10:19:10 -0400

    updated menu, fixed bugs, added hover for button

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -1,5 +1,3 @@
-package edu.stuy.starlorn.menu;
-
 import org.lwjgl.LWJGLException;
 import org.lwjgl.Sys;
 import org.lwjgl.input.Keyboard;
@@ -9,114 +7,127 @@ import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
 
 public class Menu {
-    long lastFrame;
-    int fps;
-    long lastFPS;
-
-    private Star[] _stars;
-    private Button[] _button;
-
-    public void start() {
-        try {
-            Display.setDisplayMode(new DisplayMode(1080, 720));
-            Display.create();
-        } catch (LWJGLException e) {
-            e.printStackTrace();
-            System.exit(0);
-        }
-
-        initGL();
-        getDelta();
-        lastFPS = getTime();
-
-        while (!Display.isCloseRequested()) {
-            int delta = getDelta();
-
-            update(delta);
-            renderGL();
-
-            Display.update();
-            Display.sync(60);
-        }
-
-        Display.destroy();
-    }
-
-    public void update(int delta) {
-        for(Star x: _stars){
-            x.update(delta);
-        }
-        _button[0].update(delta);
-        updateFPS();
-    }
-
-    /**
-     * Calculate how many milliseconds have passed since last frame.
-     *
-     * @return milliseconds passed since last frame
-     */
-    public int getDelta() {
-        long time = getTime();
-        int delta = (int) (time - lastFrame);
-        lastFrame = time;
-
-        return delta;
-    }
-
-    /**
-     * Get the accurate system time
-     *
-     * @return The system time in milliseconds
-     */
-    public long getTime() {
-        return (Sys.getTime() * 1000) / Sys.getTimerResolution();
-    }
-
-    /**
-     * Calculate the FPS and set it in the title bar
-     */
-    public void updateFPS() {
-        if (getTime() - lastFPS > 1000) {
-            Display.setTitle("FPS: " + fps);
-            fps = 0;
-            lastFPS += 1000;
-        }
-        fps++;
-    }
-
-    public void initGL() {
-        GL11.glMatrixMode(GL11.GL_PROJECTION);
-        GL11.glLoadIdentity();
-        GL11.glOrtho(0, 1080, 0, 720, 1, -1);
-        GL11.glMatrixMode(GL11.GL_MODELVIEW);
-        _stars = new Star[400];
-        GL11.glColor3d(1,1,1);
-        for(int i = 0; i < 400; i++){
-            _stars[i] = new Star(1080,720,.3);
-        }
-
-        GL11.glColor3d(.5,0,0);
-        _button = new Button[1];
-        _button[0] = new Button(400,300,100,100);
-    }
-
-    public void renderGL() {
-
-        // Clear The Screen And The Depth Buffer
-        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-
-        // R,G,B,A Set The Color To Blue One Time Only
-        GL11.glColor3f(1f, 1.0f, 1f);
-        for (Rectangle x: _stars){
-            x.draw();
-        }
 
 
-        if (_button[0].isPressed()){
-            GL11.glColor3d(.5, 0, 0);
-        }
-        _button[0].draw();
+	long lastFrame;
+	int fps;
+	long lastFPS;
 
-    }
+	private Star[] _stars;
+	private Button[] _button;
 
+	public void start() {
+		try {
+			Display.setDisplayMode(new DisplayMode(1080, 720));
+			Display.create();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+
+		initGL(); 
+		getDelta(); 
+		lastFPS = getTime(); 
+		
+		while (!Display.isCloseRequested()) {
+			int delta = getDelta();
+
+			update(delta);
+			renderGL();
+
+			Display.update();
+			Display.sync(60); 
+		}
+
+		Display.destroy();
+	}
+
+	public void update(int delta) {
+		for(Star x: _stars){
+			x.update(delta);
+		}
+		_button[0].update(delta);
+		updateFPS();
+	}
+
+	/**
+	 * Calculate how many milliseconds have passed since last frame.
+	 * 
+	 * @return milliseconds passed since last frame
+	 */
+	public int getDelta() {
+		long time = getTime();
+		int delta = (int) (time - lastFrame);
+		lastFrame = time;
+
+		return delta;
+	}
+
+	/**
+	 * Get the accurate system time
+	 * 
+	 * @return The system time in milliseconds
+	 */
+	public long getTime() {
+		return (Sys.getTime() * 1000) / Sys.getTimerResolution();
+	}
+
+	/**
+	 * Calculate the FPS and set it in the title bar
+	 */
+	public void updateFPS() {
+		if (getTime() - lastFPS > 1000) {
+			Display.setTitle("FPS: " + fps);
+			fps = 0;
+			lastFPS += 1000;
+		}
+		fps++;
+	}
+
+	public void initGL() {
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		GL11.glOrtho(0, 1080, 0, 720, 1, -1);
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+		_stars = new Star[400];
+		GL11.glColor3d(1,1,1);
+		for(int i = 0; i < 400; i++){
+			_stars[i] = new Star(1080,720,.3);
+		}
+		
+		GL11.glColor3d(.5,0,0);
+		_button = new Button[1];
+		_button[0] = new Button(400,300,100,100);
+
+
+		
+		
+	}
+
+	public void renderGL() {
+		  
+		// Clear The Screen And The Depth Buffer
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+
+		// R,G,B,A Set The Color To Blue One Time Only
+		GL11.glColor3f(1f, 1.0f, 1f);
+		for (Rectangle x: _stars){
+			x.draw();
+		}
+		
+		if (_button[0].isHover()){
+			GL11.glColor3d(0, .5, 0);
+		}
+		if (_button[0].isPressed()){
+			GL11.glColor3d(.5, 0, 0);
+		}
+		
+		_button[0].draw();
+		
+	}
+
+	public static void main(String[] argv) {
+		Menu menu = new Menu();
+		menu.start();
+	}
 }
--- src/menu/Rectangle.java
+++ src/menu/Rectangle.java
@@ -1,93 +1,92 @@
-package edu.stuy.starlorn.menu;
-
 import org.lwjgl.opengl.GL11;
 
 public class Rectangle {
+	
+	private double _xcor;
+	private double _ycor;
+	private double _height;
+	private double _width;
+	
+	private double _xvel;
+	private double _yvel;
+	private double _xacc;
+	private double _yacc;
+	
+	public Rectangle(double x, double y, double h, double w) {
 
-    private double _xcor;
-    private double _ycor;
-    private double _height;
-    private double _width;
+		GL11.glBegin(GL11.GL_QUADS);
+		GL11.glVertex2d(x	 , y	);
+		GL11.glVertex2d(x + w, y	);
+		GL11.glVertex2d(x + w, y + h);
+		GL11.glVertex2d(x	 , y + h);
+		GL11.glEnd();
+		
+		_xcor = x;
+		_ycor = y;
+		_height = h;
+		_width = w;
+	}
+	
+	public void draw(){
+		GL11.glBegin(GL11.GL_QUADS);
+		GL11.glVertex2d(_xcor	      , _ycor	);
+		GL11.glVertex2d(_xcor + _width, _ycor	);
+		GL11.glVertex2d(_xcor + _width, _ycor + _height);
+		GL11.glVertex2d(_xcor		  , _ycor + _height);
+		GL11.glEnd();
+	}
+	
 
-    private double _xvel;
-    private double _yvel;
-    private double _xacc;
-    private double _yacc;
-
-    public Rectangle(double x, double y, double h, double w) {
-        GL11.glColor3d(1,1,1);
-        GL11.glBegin(GL11.GL_QUADS);
-        GL11.glVertex2d(x    , y    );
-        GL11.glVertex2d(x + w, y    );
-        GL11.glVertex2d(x + w, y + h);
-        GL11.glVertex2d(x    , y + h);
-        GL11.glEnd();
-
-        _xcor = x;
-        _ycor = y;
-        _height = h;
-        _width = w;
-    }
-
-    public void draw(){
-        GL11.glColor3d(1,1,1);
-        GL11.glBegin(GL11.GL_QUADS);
-        GL11.glVertex2d(_xcor         , _ycor   );
-        GL11.glVertex2d(_xcor + _width, _ycor   );
-        GL11.glVertex2d(_xcor + _width, _ycor + _height);
-        GL11.glVertex2d(_xcor         , _ycor + _height);
-        GL11.glEnd();
-    }
-
-    public double getWidth(){return _width;}
-    public double setWidth(double Width){
-        double temp = _width;
-        _width = Width;
-        return temp;}
-    public double getHeight(){return _height;}
-    public double setHeight(double height){
-        double temp = _height;
-        _height = height;
-        return temp;}
-
-    public double getXcor(){return _xcor;}
-    public double setXcor(double xcor){
-        double temp = _xcor;
-        _xcor = xcor;
-        return temp;}
-    public double getYcor(){return _ycor;}
-    public double setYcor(double ycor){
-        double temp = _ycor;
-        _ycor = ycor;
-        return temp;}
-
-    public double getXvel(){return _xvel;}
-    public double setXvel(double xvel){
-        double temp = _xvel;
-        _xvel = xvel;
-        return temp;}
-    public double getYvel(){return _yvel;}
-    public double setYvel(double yvel){
-        double temp = _yvel;
-        _yvel = yvel;
-        return temp;}
-
-    public double getXacc(){return _xacc;}
-    public double setXacc(double xacc){
-        double temp = _xacc;
-        _xacc = xacc;
-        return temp;}
-    public double getYacc(){return _yacc;}
-    public double setYacc(double yacc){
-        double temp = _yacc;
-        _yacc = yacc;
-        return temp;}
-
-    public void update(int delta){
-        _xvel += _xacc * (double) delta / 1000;
-        _yvel += _yacc * (double) delta / 1000;
-
-        _xcor += _xvel * (double) delta / 1000;
-        _ycor += _yvel * (double) delta / 1000;
-    }
+	
+	public double getWidth(){return _width;}
+	public double setWidth(double Width){
+		double temp = _width;
+		_width = Width;
+		return temp;}
+	public double getHeight(){return _height;}
+	public double setHeight(double height){
+		double temp = _height;
+		_height = height;
+		return temp;}
+	
+	public double getXcor(){return _xcor;}
+	public double setXcor(double xcor){
+		double temp = _xcor;
+		_xcor = xcor;
+		return temp;}
+	public double getYcor(){return _ycor;}
+	public double setYcor(double ycor){
+		double temp = _ycor;
+		_ycor = ycor;
+		return temp;}
+	
+	public double getXvel(){return _xvel;}
+	public double setXvel(double xvel){
+		double temp = _xvel;
+		_xvel = xvel;
+		return temp;}
+	public double getYvel(){return _yvel;}
+	public double setYvel(double yvel){
+		double temp = _yvel;
+		_yvel = yvel;
+		return temp;}
+	
+	public double getXacc(){return _xacc;}
+	public double setXacc(double xacc){
+		double temp = _xacc;
+		_xacc = xacc;
+		return temp;}
+	public double getYacc(){return _yacc;}
+	public double setYacc(double yacc){
+		double temp = _yacc;
+		_yacc = yacc;
+		return temp;}
+	
+	public void update(int delta){
+		_xvel += _xacc * (double) delta / 1000;
+		_yvel += _yacc * (double) delta / 1000;
+		
+		_xcor += _xvel * (double) delta / 1000;
+		_ycor += _yvel * (double) delta / 1000;
+	}
 }
