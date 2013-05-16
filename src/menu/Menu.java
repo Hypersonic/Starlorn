a928ced9396bd3d7fc167e974452bec9ddfe306a
Ben Kurtovic <ben.kurtovic@verizon.net>
2013-05-15 20:28:04 -0400

    Convert tabs to spaces; re-add package statements; cleanup for readability.

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -1,3 +1,5 @@
+package edu.stuy.starlorn.menu;
+
 import org.lwjgl.LWJGLException;
 import org.lwjgl.Sys;
 import org.lwjgl.input.Keyboard;
@@ -8,126 +10,116 @@ import org.lwjgl.opengl.GL11;
 
 public class Menu {
 
+    long lastFrame;
+    int fps;
+    long lastFPS;
 
-	long lastFrame;
-	int fps;
-	long lastFPS;
+    private Star[] _stars;
+    private Button[] _button;
 
-	private Star[] _stars;
-	private Button[] _button;
+    public void start() {
+        try {
+            Display.setDisplayMode(new DisplayMode(1080, 720));
+            Display.create();
+        } catch (LWJGLException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
 
-	public void start() {
-		try {
-			Display.setDisplayMode(new DisplayMode(1080, 720));
-			Display.create();
-		} catch (LWJGLException e) {
-			e.printStackTrace();
-			System.exit(0);
-		}
+        initGL();
+        getDelta();
+        lastFPS = getTime();
 
-		initGL(); 
-		getDelta(); 
-		lastFPS = getTime(); 
-		
-		while (!Display.isCloseRequested()) {
-			int delta = getDelta();
+        while (!Display.isCloseRequested()) {
+            int delta = getDelta();
 
-			update(delta);
-			renderGL();
+            update(delta);
+            renderGL();
 
-			Display.update();
-			Display.sync(60); 
-		}
+            Display.update();
+            Display.sync(60);
+        }
 
-		Display.destroy();
-	}
+        Display.destroy();
+    }
 
-	public void update(int delta) {
-		for(Star x: _stars){
-			x.update(delta);
-		}
-		_button[0].update(delta);
-		updateFPS();
-	}
+    public void update(int delta) {
+        for(Star x: _stars){
+            x.update(delta);
+        }
+        _button[0].update(delta);
+        updateFPS();
+    }
 
-	/**
-	 * Calculate how many milliseconds have passed since last frame.
-	 * 
-	 * @return milliseconds passed since last frame
-	 */
-	public int getDelta() {
-		long time = getTime();
-		int delta = (int) (time - lastFrame);
-		lastFrame = time;
+    /**
+     * Calculate how many milliseconds have passed since last frame.
+     *
+     * @return milliseconds passed since last frame
+     */
+    public int getDelta() {
+        long time = getTime();
+        int delta = (int) (time - lastFrame);
+        lastFrame = time;
 
-		return delta;
-	}
+        return delta;
+    }
 
-	/**
-	 * Get the accurate system time
-	 * 
-	 * @return The system time in milliseconds
-	 */
-	public long getTime() {
-		return (Sys.getTime() * 1000) / Sys.getTimerResolution();
-	}
+    /**
+     * Get the accurate system time
+     *
+     * @return The system time in milliseconds
+     */
+    public long getTime() {
+        return (Sys.getTime() * 1000) / Sys.getTimerResolution();
+    }
 
-	/**
-	 * Calculate the FPS and set it in the title bar
-	 */
-	public void updateFPS() {
-		if (getTime() - lastFPS > 1000) {
-			Display.setTitle("FPS: " + fps);
-			fps = 0;
-			lastFPS += 1000;
-		}
-		fps++;
-	}
+    /**
+     * Calculate the FPS and set it in the title bar
+     */
+    public void updateFPS() {
+        if (getTime() - lastFPS > 1000) {
+            Display.setTitle("FPS: " + fps);
+            fps = 0;
+            lastFPS += 1000;
+        }
+        fps++;
+    }
 
-	public void initGL() {
-		GL11.glMatrixMode(GL11.GL_PROJECTION);
-		GL11.glLoadIdentity();
-		GL11.glOrtho(0, 1080, 0, 720, 1, -1);
-		GL11.glMatrixMode(GL11.GL_MODELVIEW);
-		_stars = new Star[400];
-		GL11.glColor3d(1,1,1);
-		for(int i = 0; i < 400; i++){
-			_stars[i] = new Star(1080,720,.3);
-		}
-		
-		GL11.glColor3d(.5,0,0);
-		_button = new Button[1];
-		_button[0] = new Button(400,300,100,100);
+    public void initGL() {
+        GL11.glMatrixMode(GL11.GL_PROJECTION);
+        GL11.glLoadIdentity();
+        GL11.glOrtho(0, 1080, 0, 720, 1, -1);
+        GL11.glMatrixMode(GL11.GL_MODELVIEW);
+        _stars = new Star[400];
+        GL11.glColor3d(1,1,1);
+        for(int i = 0; i < 400; i++){
+            _stars[i] = new Star(1080,720,.3);
+        }
 
+        GL11.glColor3d(.5,0,0);
+        _button = new Button[1];
+        _button[0] = new Button(400,300,100,100);
+    }
 
-		
-		
-	}
+    public void renderGL() {
 
-	public void renderGL() {
-		  
-		// Clear The Screen And The Depth Buffer
-		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+        // Clear The Screen And The Depth Buffer
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 
-		// R,G,B,A Set The Color To Blue One Time Only
-		GL11.glColor3f(1f, 1.0f, 1f);
-		for (Rectangle x: _stars){
-			x.draw();
-		}
-		
-		if (_button[0].isHover()){
-			GL11.glColor3d(0, .5, 0);
-		}
-		if (_button[0].isPressed()){
-			GL11.glColor3d(.5, 0, 0);
-		}
-		
-		_button[0].draw();
-		
-	}
+        // R,G,B,A Set The Color To Blue One Time Only
+        GL11.glColor3f(1f, 1.0f, 1f);
+        for (Rectangle x: _stars){
+            x.draw();
+        }
 
-	public static void main(String[] argv) {
-		Menu menu = new Menu();
-		menu.start();
-	}
+        if (_button[0].isHover()){
+            GL11.glColor3d(0, .5, 0);
+        }
+        if (_button[0].isPressed()){
+            GL11.glColor3d(.5, 0, 0);
+        }
+
+        _button[0].draw();
+
+    }
 }
