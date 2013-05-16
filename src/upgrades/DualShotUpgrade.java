74e764cf3a9b2bfd83b56a5d0f265f8cfa4055ae
Josh Hofing <joshhofing@gmail.com>
2013-05-16 12:41:30 -0400

    Add DualShotUpgrade, 2 shots next to each other.

---
+++ src/upgrades/DualShotUpgrade.java
@@ -0,0 +1,28 @@
+package edu.stuy.starlorn.upgrades;
+
+public class DualShotUpgrade extends GunUpgrade {
+    private boolean _right;
+
+    public DualShotUpgrade() {
+        super();
+        _right = true;
+    }
+
+    @Override
+    public int getNumShots() {
+        return 2;
+    }
+
+    @Override
+    public double getXOffset() {
+        int direction = 0;
+        if (_right) {
+            _right = !_right;
+            direction = 1;
+        } else if (!_right) {
+            _right = !_right;
+            direction = -1;
+        }
+        return direction * 10;
+    }
+}
