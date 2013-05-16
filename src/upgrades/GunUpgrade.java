e8465687694a553986a766047c3db5a27b33767f
Josh Hofing <joshhofing@gmail.com>
2013-05-16 11:39:25 -0400

    Remove useless comment in GunUpgrade

--- src/upgrades/GunUpgrade.java
+++ src/upgrades/GunUpgrade.java
@@ -12,7 +12,7 @@ public class GunUpgrade extends Upgrade {
 
     /*
      * Returns the number of shots this upgrade allows for.
-     * The player's ship should choose the highest of these from its upgrades.
+     * Highest should be chosen
      */
     public int getNumShots() {
         return 1;
@@ -20,16 +20,17 @@ public class GunUpgrade extends Upgrade {
     /*
      * Gives an angle this ship should aim at, measured in radians,
      * From the vertical axis.
-     * It should use the value given by the upgrade with the highest numShots
      * Called for each shot, so it should probably keep track of which one it's at.
+     * Should be chosen based on getNumShots
      */
     public double getAimAngle() {
-        return 0; // Aim straight.
+        return 0;
     }
     /*
      * Gives a number of coordinates to offset the shot's origin
      * on the X axis from wherever shots would normally come from.
      * Not garunteed to return the same thing each time, should probably keep track of which shot this is (like getAimAngle)
+     * Should be chosen based off of getNumShots
      */
     public int getXOffset() {
         return 0;
@@ -37,6 +38,7 @@ public class GunUpgrade extends Upgrade {
     /* 
      * Given the amount of damage that would be done, tell how much should be done.
      * Multipliers, buffs, etc go here.
+     * All should be applied
      */
     public int getDamage(int damage) {
         return damage;
