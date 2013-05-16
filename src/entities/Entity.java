07a4123b88a9a25cd64f509692c0cbe44f12ce5a
Victor Jiao <VjiaoBlack@gmail.com>
2013-05-16 11:00:38 -0400

    yep

--- src/entities/Entity.java
+++ src/entities/Entity.java
@@ -5,12 +5,15 @@ package edu.stuy.starlorn.entities;
  * It should be subclassed and extended.
  */
 public class Entity {
-    private double _x, _y;
+    private double _xcor, _ycor;
+    private double _xvel, _yvel;
+    
     private int _width, _height;
 
     public Entity(double x, double y, int width, int height) {
-        _x = x;
-        _y = y;
+        _xcor = x;
+        _ycor = y;
+        _xvel = _yvel = 0;
         _width = width;
         _height = height;
     }
@@ -23,60 +26,61 @@ public class Entity {
         this(0,0);
     }
 
+    public void draw() { //this draws the thing
+        //code to be put later for texture
+    }
+    
     /*
      * Used for handling things that need to be done before anyone takes action.
-     */
+     
+     // (these functions aren't useful, it should be all in the update() )
+
+     
     public void preStep() {
         System.out.printf("X: %.1f, Y: %.1f\n",getX(), getY());
     }
 
-    /*
+    
      * Used for taking actions (moving, shooting, etc)
-     */
+     
+     // (these functions aren't useful, it should be all in the update() )
+    
     public void step() {
         setXY(getX()-1 + Math.random() * 2, getY()-1 + Math.random() * 2);
     }
 
-    /*
+    
      * Used for cleanup after the step
-     */
+     
     public void postStep() {
+        
     }
+     */
 
-    public void setX(double x) {
-        _x = x;
-    }
+    public void setX(double x) {_xcor = x;}
+    public double getX() {return _xcor;}
 
-    public double getX() {
-        return _x;
-    }
+    public void setY(double y) {_ycor = y;}
+    public double getY() {return _ycor;}
 
-    public void setY(double y) {
+    public void setWidth(int width) {_width = width;}
+    public int getWidth() {return _width;}
 
-        _y = y;
-    }
-    public double getY() {
-        return _y;
-    }
+    public void setHeight(int height) {_height = height;}
+    public int getHeight() {return _height;}
 
-    public void setWidth(int width) {
-        _width = width;
+    public void setXY(double x, double y) {setX(x);setY(y);}
+    
+    public boolean intersect(/* Some object, idk*/){ //to be coded later
     }
+    
+    public void update(int delta){
+        _xcor += _xvel;
+        _ycor += _yvel;
 
-    public int getWidth() {
-        return _width;
+        if (intersect) {//do stuff
+        }
     }
-
-    public void setHeight(int height) {
-        _height = height;
-    }
-
-    public int getHeight() {
-        return _height;
-    }
-
-    public void setXY(double x, double y) {
-        setX(x);
-        setY(y);
-    }
+    
+    
 }
