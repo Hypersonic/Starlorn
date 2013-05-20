ca5891184595443dba0aeb2f6da05d667e9a44a1
Josh Hofing <joshhofing@gmail.com>
2013-05-20 10:19:22 -0400

    Add description string to Upgrade.

--- src/upgrades/Upgrade.java
+++ src/upgrades/Upgrade.java
@@ -4,6 +4,17 @@ package edu.stuy.starlorn.upgrades;
  * A generic upgrade. Modifies aspects of a ship
  */
 public class Upgrade {
+    private String _description;
+
     public Upgrade() {
+        _description = "Upgrade!";
+    }
+
+    /*
+     * Get a description to pop up when the upgrade is gathered.
+     * Should be quick, and maybe funny
+     */
+    public String getDescription() {
+        return _description;
     }
 }
