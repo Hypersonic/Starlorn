83cf5fc16142666980c0e1a47e3d8eaa2869d21d
Josh Hofing <joshhofing@gmail.com>
2013-05-23 10:52:21 -0400

    More work on ship, most of shoot finished.

--- src/entities/Ship.java
+++ src/entities/Ship.java
@@ -6,10 +6,14 @@ import java.util.LinkedList;
 
 public class Ship extends Entity {
     protected LinkedList<GunUpgrade> _gunupgrades;
+    protected int _baseDamage, _baseShotSpeed, _health;
 
     public Ship() {
         super();
         _gunupgrades = new LinkedList<GunUpgrade>();
+        _baseDamage = 1;
+        _baseShotSpeed = 1;
+        _health = 10;
     }
 
     public void addUpgrade(GunUpgrade upgrade) {
@@ -20,6 +24,15 @@ public class Ship extends Entity {
      * Create the shots based on the available GunUpgrades
      */
     public void shoot() {
-        //TODO: Make this work based off the GunUpgrade spec
+        GunUpgrade topShot = _gunupgrades.get(0);
+        int damage = _baseDamage;
+        int shotSpeed = _baseShotSpeed;
+        for (GunUpgrade up : _gunupgrades) {
+            if (up.getNumShots() > topShot.getNumShots())
+                topShot = up;
+            damage = up.getDamage(damage);
+            shotSpeed = up.getShotSpeed(shotSpeed);
+        }
+        // Create new shots, based on dem vars
     }
 }
