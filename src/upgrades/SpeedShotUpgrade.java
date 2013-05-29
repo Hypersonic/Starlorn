3456c9231e1841898147be692e2c554ba74b0752
Josh Hofing <joshhofing@gmail.com>
2013-05-29 13:08:35 -0400

    Add SpeedShot upgrade. Doubles shot speed.

---
+++ src/upgrades/SpeedShotUpgrade.java
@@ -0,0 +1,14 @@
+package edu.stuy.starlorn.upgrades;
+
+public class SpeedShotUpgrade extends GunUpgrade {
+    public SpeedShotUpgrade() {
+        super();
+        _name = "Speed Shot";
+        _description = "Shot Speed x 2!";
+    }
+
+    @Override
+    public int getShotSpeed(int shotspeed) {
+        return shotspeed * 2;
+    }
+}
