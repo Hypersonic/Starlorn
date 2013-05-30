7f58ef6d94733e15699dadc7d3c4dc0f17d61b39
Josh Hofing <joshhofing@gmail.com>
2013-05-30 10:37:21 -0400

    Allow for dificulty input in generators
    write generateLevel

--- src/util/Generator.java
+++ src/util/Generator.java
@@ -22,15 +22,42 @@ public class Generator {
         return generatePath(10);
     }
 
-    public static EnemyShip generateEnemy() {
-        EnemyShip enemy = new EnemyShip();
+    public static Wave generateWave(int dificulty) {
+        Wave wave = new Wave();
+        int numEnemies = (int) (Math.random() * dificulty * dificulty);
+        EnemyShip enemyType = generateEnemy(dificulty);
         Path path = generatePath();
-        int damage = (int) (Math.random() * 4); 
-        int shotSpeed = (int) (Math.random() * 4);
-        int health = (int) (Math.random() * 20);
-        int cooldown = (int) (Math.random() * 20);
-        int cooldownRate = (int) (Math.random() * 5);
-        int movementSpeed = (int) (Math.random() * 4);
+        wave.setPath(path);
+        wave.setEnemyType(enemyType);
+        wave.setNumEnemies(numEnemies);
+        return wave;
+    }
+
+    public static Wave generateWave() {
+        return generateWave(1);
+    }
+
+    public static Level generateLevel(int numWaves, int dificulty) {
+        Level level = new Level();
+        for (int i = 0; i < numWaves; i++) {
+            level.addWave(generateWave(dificulty));
+        }
+        return level;
+    }
+
+    public static Level generateLevel() {
+        return generateLevel(1, 1);
+    }
+
+    public static EnemyShip generateEnemy(int dificulty) {
+        EnemyShip enemy = new EnemyShip();
+        Path path = generatePath(dificulty + 5);
+        int damage = (int) (Math.random() * dificulty); 
+        int shotSpeed = (int) (Math.random() * dificulty);
+        int health = (int) (Math.random() * dificulty * 10);
+        int cooldown = (int) (Math.random() * (100 / dificulty) + 5);
+        int cooldownRate = (int) (Math.random() * Math.log(dificulty)); // increase the cooldown rate logarithmically
+        int movementSpeed = (int) (Math.random() * dificulty);
         enemy.setPath(path);
         enemy.setBaseDamage(damage);
         enemy.setBaseShotSpeed(shotSpeed);
@@ -39,5 +66,9 @@ public class Generator {
         enemy.setCooldownRate(cooldownRate);
         enemy.setMovementSpeed(movementSpeed);
         return enemy;
+    }
+
+    public static EnemyShip generateEnemy() {
+        return generateEnemy(1);
     }
 }
