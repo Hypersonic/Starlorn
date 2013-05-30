ad43e53f6670083b71e2c64641c7686e459ec2b6
Victor Jiao <VjiaoBlack@gmail.com>
2013-05-30 08:32:18 -0400

    removed error log cuz yeah

--- src/Starlorn.java
+++ src/Starlorn.java
@@ -1,11 +1,14 @@
 package edu.stuy.starlorn;
-
+import java.io.IOException;
 import edu.stuy.starlorn.display.Screen;
 // import edu.stuy.starlorn.entities.Projectile;
 import edu.stuy.starlorn.menu.Menu;
 // import edu.stuy.starlorn.world.World;
+import edu.stuy.starlorn.util.Preferences;
 
 public class Starlorn {
+	private Preferences _preferences;
+	
     public static void main(String[] args) {
         Screen screen = new Screen();
         screen.setup();
@@ -29,4 +32,18 @@ public class Starlorn {
 
         screen.shutdown();
     }
+    
+    public void init() {
+    	try{
+    	_preferences = new Preferences();
+    	_preferences.init();
+    	_preferences.writePreferences();
+    	}
+    	catch (IOException e){
+    		System.out.println("whoops");
+    	}
+    }
+    
+    
+    
 }
