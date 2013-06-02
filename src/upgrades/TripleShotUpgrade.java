18dd925f88f41f6eae852578f808c34563a10619
Josh Hofing <joshhofing@gmail.com>
2013-06-01 20:28:40 -0400

    Add TripleShotUpgrade

---
+++ src/upgrades/TripleShotUpgrade.java
@@ -0,0 +1,25 @@
+package edu.stuy.starlorn.upgrades;
+
+public class TripleShotUpgrade extends GunUpgrade {
+    private int _shotNum;
+
+    public TripleShotUpgrade() {
+        super();
+        _shotNum = 0;
+        _name = "Triple shot";
+        _description = "";
+    }
+
+    @Override
+    public int getNumShots() {
+        return 3;
+    }
+
+    @Override
+    public double getAimAngle() {
+        int direction = _shotNum - 1;
+        _shotNum++;
+        if (_shotNum > 2) _shotNum = 0;
+        return direction * Math.PI/8;
+    }
+}
