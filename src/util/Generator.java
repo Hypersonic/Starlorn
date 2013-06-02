bda866eea6f3c01e7a6243b06e5fcae32978e5e7
Josh Hofing <joshhofing@gmail.com>
2013-06-02 14:55:03 -0400

    Tweaks and changes to enemy generation

--- src/util/Generator.java
+++ src/util/Generator.java
@@ -4,31 +4,29 @@ import edu.stuy.starlorn.world.Path;
 import edu.stuy.starlorn.world.Wave;
 import edu.stuy.starlorn.world.Level;
 import edu.stuy.starlorn.entities.EnemyShip;
+import edu.stuy.starlorn.upgrades.*;
 
 public class Generator {
     public static Path generatePath(int numPoints) {
+        numPoints += 2;
         Path p = new Path();
         int screenWidth = Preferences.getValue("screenWidth");
-        int screenHeight = Preferences.getValue("screenHeight");
+        int screenHeight = Preferences.getValue("screenHeight") / 2;
         int firstx = (int) Math.round(Math.random()) * screenWidth; // Spawn us at one edge
         int firsty = (int) (Math.random() * screenHeight);
         p.addCoords(firstx, firsty);
-        int prevx = firstx;
-        int prevy = firsty;
         for (int i = 0; i < numPoints; i++) {
             // Pick locations within the size of the screen / 10 nearby the previous value, and make sure it's in the range of the screen
-            int x = (prevx + (int) ((Math.random() - .5) * (screenWidth/10))) % screenWidth;
-            int y = (prevy + (int) ((Math.random() - .5) * (screenHeight/10))) % screenHeight;
+            int x = (int) (Math.random() * screenWidth);
+            int y = (int) (Math.random() * screenHeight);
             p.addCoords(x, y);
-            prevx = x;
-            prevy = y;
         }
         p.addCoords(Math.abs(firstx - screenWidth), (int) (Math.random() *screenHeight)); // Add a point somewhere on the opposite side of spawn
         return p;
     }
 
     public static Path generatePath() {
-        return generatePath(10);
+        return generatePath(1000);
     }
 
     public static Wave generateWave(int difficulty) {
@@ -59,14 +57,23 @@ public class Generator {
     }
 
     public static EnemyShip generateEnemy(int difficulty) {
+        System.out.println("difficulty: " + difficulty);
         Path path = generatePath(difficulty + 5);
         EnemyShip enemy = new EnemyShip(path);
-        int damage = (int) (Math.random() * difficulty);
-        int shotSpeed = (int) (Math.random() * difficulty);
-        int health = (int) (Math.random() * difficulty * 10);
-        int cooldown = (int) (Math.random() * (100 / difficulty) + 5);
-        int cooldownRate = (int) (Math.random() * Math.log(difficulty)); // increase the cooldown rate logarithmically
-        int maxSpeed = (int) (Math.random() * difficulty);
+        int damage = (int) (1 + Math.random() * difficulty);
+        int shotSpeed = (int) (1 + Math.random() * difficulty) + 10;
+        int health = (int) (1 + Math.random() * difficulty * 10);
+        int cooldown = (int) (Math.random() * (100 / difficulty) + 15);
+        int cooldownRate = (int) Math.sqrt(difficulty) + 1;
+        int maxSpeed = (int) Math.ceil(Math.random() * Math.log(difficulty)) * 5;
+        if (Math.random() < .01)
+            enemy.addUpgrade(new ScatterShotUpgrade());
+        if (Math.random() < .01)
+            enemy.addUpgrade(new TripleShotUpgrade());
+        if (Math.random() < .01)
+            enemy.addUpgrade(new DoubleShotUpgrade());
+        if (Math.random() < .01)
+            enemy.addUpgrade(new DualShotUpgrade());
         enemy.setBaseDamage(damage);
         enemy.setBaseShotSpeed(shotSpeed);
         enemy.setMaxHealth(health);
