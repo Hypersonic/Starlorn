d96c2e3fa42adc5de13de9afee39c192bab5c573
VjiaoBlack <VjiaoBlack@gmail.com>
2013-06-02 23:33:34 -0300

    Update LawnSprinklerUpgrade.java
    finished the code for lawnsprinkler

--- src/upgrades/LawnSprinklerUpgrade.java
+++ src/upgrades/LawnSprinklerUpgrade.java
@@ -1,2 +1,40 @@
-public class LawnSprinlerUpgrade{
+package edu.stuy.starlorn.upgrades;
+
+public class LawnSprinklerUpgrade extends GunUpgrade {
+  
+    private double _direction;
+
+    public LawnSprinklerUpgrade() {
+        super();
+        _name = ".¸¸.·´¯`·.¸.Lawn Sprinkler!!!.¸¸.·´¯`·.¸.";
+        _description = "wat r u srs";
+        
+    }
+    
+    @Override
+    public int getNumShots() {
+        return 1;
+    }
+
+    @Override
+    public double getAimAngle() {
+        if (_direction >= 2){
+          _direction = 0;
+        }
+        else 
+          _direction += 0.1;
+        // Random angle going forwards
+        return (Math.PI * (1 - .5));
+    }
+
+    @Override
+    public int getShotSpeed(int shotspeed) {
+        // Random speed, either 1, 2, or 3
+        return (int) 5 * shotspeed;
+    }
+
+    @Override
+    public Upgrade clone() {
+        return new LawnSprinklerUpgrade();
+    }
 }
