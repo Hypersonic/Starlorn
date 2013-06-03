36c9ac3e8b73d33015dfd6aedcfa384627b31edc
VjiaoBlack <VjiaoBlack@gmail.com>
2013-06-03 00:06:12 -0300

    Update LawnSprinklerUpgrade.java
    ok it works now yay
    @ben if i overrode something sry just tell me
    brb

--- src/upgrades/LawnSprinklerUpgrade.java
+++ src/upgrades/LawnSprinklerUpgrade.java
@@ -1,25 +1,36 @@
 package edu.stuy.starlorn.upgrades;
 
 public class LawnSprinklerUpgrade extends GunUpgrade {
-
+  
     private double _direction;
 
     public LawnSprinklerUpgrade() {
         super();
-        _name = "Lawn Sprinkler";
-        _description = "¸.·´¯`·.´¯`·.¸¸.·´¯`·.¸";
-
+        _name = ".¸¸.·´¯`·.¸.Lawn Sprinkler!!!.¸¸.·´¯`·.¸.";
+        _description = "wat r u srs";
+        
+    }
+    
+    @Override
+    public int getNumShots() {
+        return 10;
     }
 
     @Override
     public double getAimAngle() {
-        if (_direction >= 2){
-            _direction = 0;
+        if (_direction >= 2.1){
+          _direction = _direction - 1.8;
         }
-        else
-            _direction += 0.1;
+        else 
+          _direction += 0.5;
         // Random angle going forwards
-        return (Math.PI * (1 - _direction) / 2);
+        return (Math.PI * (1 - _direction));
+    }
+
+    @Override
+    public int getShotSpeed(int shotspeed) {
+        // Random speed, either 1, 2, or 3
+        return (int) shotspeed;
     }
 
     @Override
